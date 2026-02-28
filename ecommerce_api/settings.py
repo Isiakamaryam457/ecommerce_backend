5e08088d99cf5ae50308193e2077906e9a6fb079
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-02-28 15:56:40 +0100

    allowing users to submit reviews

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -48,7 +48,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'DEFAULT_PAGINATION_CLASS': 'products.pagination.CustomPagination',
     'PAGE_SIZE': 10,
 }
 
