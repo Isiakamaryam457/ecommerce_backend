3d5591cb98cd0efff1f980c3ab7297f47e1c5ea6
Isiaka Maryam <isiakamaryam457@gmail.com>
2026-03-01 22:54:32 +0100

    adding the final commit

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -24,4 +26,4 @@ urlpatterns = [
     path('api/products/', include('products.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),       # login
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'), 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # serves media files in development
